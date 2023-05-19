9043db3630ab4dea1663a8f142f0067f6b1c3769
justina177 <justinaenenche@yahoo.com>
2023-05-19 09:28:09 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+import Register from "./Register";
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <Register />
+    
+        
+      
+    
+  )
+}
+
+export default App
